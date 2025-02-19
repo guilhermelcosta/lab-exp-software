f713271b3648ef4c22bf2aac10d516337bbed553
Guilherme Costa <guilhermeldcosta@gmail.com>
2025-02-18 21:38:14 -0300

    feat: paginacao ate 1000 repositorios

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import os
-import requests
+from http.client import responses
+
 from gql import gql, Client
 from gql.transport.requests import RequestsHTTPTransport
 
 query = gql("""
-{
-  search(query: "stars:>1", type: REPOSITORY, first: 100) {
+query ($first: Int!, $after: String) {
+  search(query: "stars:>1", type: REPOSITORY, first: $first, after: $after) {
     edges {
       node {
         ... on Repository {
@@ -39,24 +40,44 @@ query = gql("""
         }
       }
     }
+    pageInfo {
+      hasNextPage
+      endCursor
+    }
   }
 }
 """)
 
 
 def setup_github_client():
-    transport = RequestsHTTPTransport(url='https://api.github.com/graphql',
-                                      headers={'Authorization': f'Bearer {GITHUB_TOKEN}'},
-                                      use_json=True)
-
+    transport = RequestsHTTPTransport(
+        url='https://api.github.com/graphql',
+        headers={
+            'Authorization': f'Bearer TOKEN'},
+        use_json=True
+    )
     return Client(transport=transport, fetch_schema_from_transport=True)
 
 
 def main():
     client = setup_github_client()
-    response = client.execute(query)
-    print(response)
+    has_next = True
+    cursor = None
+    repositories_count = 0
 
+    while has_next and repositories_count <= 1000:
+        response = client.execute(query, variable_values={'first': 10, 'after': cursor})
+        repositories = response['search']['edges']
+        repositories_count += len(repositories)
+        page_info = response['search']['pageInfo']
+        has_next = page_info['hasNextPage']
+        cursor = page_info['endCursor']
+        print(repositories_count)
+
+    # print(response)
+
+
+# response = client.execute(query, variable_values={'first': 10})
 
 if __name__ == '__main__':
     main()
